4c7740be12e6dcbd10d0380c74fddb6a8eaa8321
xshoker <65734637+xshoker@users.noreply.github.com>
2021-09-15 22:32:08 +0300

    XShoker

--- homework_01/main.py
+++ homework_01/main.py
@@ -8,26 +8,16 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def is_odd(num):
-    return num % 2 != 0
-
-
-def is_even(num):
-    return num % 2 == 0
-
-
 def is_prime(num):
     for i2 in range(2, num):
         if not num % i2:
             return False
     return True
 
-
-
 def filter_numbers(nums, factor):
     if factor == ODD:
-        return list(filter(is_odd, nums))
+        return list(filter(lambda x: x % 2 != 0, nums))
     if factor == EVEN:
-        return list(filter(is_even, nums))
+        return list(filter(lambda x: x % 2 == 0, nums))
     if factor == PRIME:
         return list(filter(is_prime, nums))
