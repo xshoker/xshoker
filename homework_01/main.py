85678fd2ea799ccb68ac74daf98a71e8e7e38cfc
xshoker <65734637+xshoker@users.noreply.github.com>
2021-09-15 20:13:42 +0300

    XShoker

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,32 +1,29 @@
-"""
-Домашнее задание №1
-Функции и структуры данных
-"""
+def power_numbers(*nums):
+    return [num**2 for num in nums]
+print(power_numbers(1, 2, 5, 7))
 
-
-def power_numbers():
-    """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
-    """
-
-
-# filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def is_ODD(num):
+    return num%2 == 0
 
-def filter_numbers():
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
+def is_EVEN(num):
+    return num%2 != 0
 
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
-    """
+def is_PRIME(num):
+    for i2 in range(2, i):
+        if num//i2:
+            return False
+    return True
+
+def filter_numbers(nums, factor):
+    if factor == ODD:
+        return list(filter(is_ODD, nums))
+    if factor == EVEN:
+        return list(filter(is_EVEN, nums))
+    if factor == PRIME:
+        return list(filter(is_PRIME, nums))
+
+print(filter_numbers(list(range(0, 40)), ODD))
