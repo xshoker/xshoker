0af7369da367519fa8bfffcf3d2ba3a1679e9288
xshoker <65734637+xshoker@users.noreply.github.com>
2022-01-31 12:50:34 +0300

    XShoker

--- homework_04/jsonplaceholder_requests.py
+++
@@ -1,6 +0,0 @@
-"""
-создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
-"""
-
-USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
-POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
---
+++ homework_05/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+from views.products import products_app
+
+app = Flask(__name__)
+app.register_blueprint(products_app, url_prefix="/products")
+
+
+@app.route("/")
+def root():
+    return '<h1>Hello, World!</h1>'
+
+
+@app.route("/items/")
+@app.route("/items/<int:item_id>")
+def get_item_by_id(item_id=None):
+    return {'item_id': item_id}
+
+
+if __name__=='__main__':
+    app.run(debug=True)
